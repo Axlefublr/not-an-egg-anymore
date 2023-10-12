0dc30cd59bc1b495b439f473627aa0412c7470ba
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-12 21:38:22 +0800

    impl traits for T

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,5 +1,3 @@
-use std::time;
-
 mod traits;
 mod future;
 
--- 1_concepts/1_2_box_pin/src/traits.rs
+++ 1_concepts/1_2_box_pin/src/traits.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::ops;
 use std::pin::Pin;
 
 trait SayHi: fmt::Debug {
@@ -8,12 +9,44 @@ trait SayHi: fmt::Debug {
 }
 
 trait MutMeSomehow {
+    fn mut_me_somehow(self: Pin<&mut Self>);
+}
+
+impl<T: fmt::Debug> SayHi for T {}
+
+struct Calc(i32);
+
+impl Calc {
+    fn increment(&mut self) {
+        self.0 += 1;
+    }
+}
+
+struct Wrapper<T>(T);
+
+impl<T> Wrapper<T> {
+    fn new(inner: T) -> Self {
+        Self(inner)
+    }
+}
+
+impl MutMeSomehow for Wrapper<Calc> {
     fn mut_me_somehow(self: Pin<&mut Self>) {
-        // Implementation must be meaningful, and
-        // obviously call something requiring `&mut self`.
-        // The point here is to practice dealing with
-        // `Pin<&mut Self>` -> `&mut self` conversion
-        // in different contexts, without introducing
-        // any `Unpin` trait bounds.
+        let this = unsafe { self.get_unchecked_mut() };
+        this.0.increment();
+    }
+}
+
+impl<T> ops::Deref for Wrapper<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> ops::DerefMut for Wrapper<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
