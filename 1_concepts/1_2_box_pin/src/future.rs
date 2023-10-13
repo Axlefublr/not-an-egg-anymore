89e1ced3b1e1c352fcc029f8695ac149a1417bac
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-13 21:14:41 +0800

    use pin_project to simplify Pin projection

--- 1_concepts/1_2_box_pin/src/future.rs
+++ 1_concepts/1_2_box_pin/src/future.rs
@@ -2,38 +2,30 @@ use std::future::Future;
 use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
+use std::time::Duration;
 use std::time::Instant;
+use pin_project::pin_project;
 
+#[pin_project]
 struct MeasurableFuture<Fut: Future> {
+    #[pin]
     inner_future: Fut,
     started_at: Option<Instant>,
 }
 
 impl<Fut: Future> Future for MeasurableFuture<Fut> {
-    type Output = Fut::Output;
+    type Output = (Fut::Output, Duration);
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // Safety: projecting to a field of this struct is safe
-        let measurable_future = unsafe { self.get_unchecked_mut() };
+        let mut this = self.project();
 
-        // Safety: this directly re-polls the inner future and doesn't move it.
-        let inner_fut = unsafe { Pin::new_unchecked(&mut measurable_future.inner_future) };
+        let start = this.started_at.get_or_insert_with(Instant::now);
+        let inner_poll = this.inner_future.as_mut().poll(cx);
+        let elapsed = start.elapsed();
 
-        match inner_fut.poll(cx) {
-            Poll::Ready(output) => {
-                let duration = measurable_future
-                    .started_at
-                    .unwrap_or_else(Instant::now)
-                    .elapsed();
-                println!("Execution time in nanoseconds: {}", duration.as_nanos());
-                Poll::Ready(output)
-            }
-            Poll::Pending => {
-                if measurable_future.started_at.is_none() {
-                    measurable_future.started_at = Some(Instant::now());
-                }
-                Poll::Pending
-            }
+        match inner_poll {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(output) => Poll::Ready((output, elapsed)),
         }
     }
 }
