de27dafe77478ce852470ee83bad9aa833d9a8f6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-16 18:39:23 +0800

    add simple stack implementation

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,3 +1,20 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+
 fn main() {
     println!("Implement me!");
+}
+
+struct GlobalStack<T>(Arc<Mutex<Vec<T>>>);
+
+impl<T> GlobalStack<T> {
+    pub fn add(&self, value: T) {
+        let mut vector = self.0.lock().unwrap();
+        vector.push(value);
+    }
+
+    pub fn pop(&self) -> Option<T> {
+        let mut vector = self.0.lock().unwrap();
+        vector.pop()
+    }
 }
