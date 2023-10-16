891eff1179c369fe85990ea22ff55839ce9a45f1
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-16 18:03:50 +0800

    use tokio to give a fully working future await example

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,5 +1,4 @@
 use std::future::Future;
-use std::process::Output;
 use std::thread;
 use std::time::Duration;
 
@@ -8,10 +7,9 @@ use future::MeasurableFuture;
 mod future;
 mod traits;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let to_measure = MeasurableFuture::new(example());
-    // Imagine that we're using an async runtime to do this
-    // Not included because it's not the point of the example
     let (_, duration) = to_measure.await;
     println!("Took {} nanoseconds", duration.as_nanos());
 }
