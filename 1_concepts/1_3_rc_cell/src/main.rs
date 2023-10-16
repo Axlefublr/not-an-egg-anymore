3343c3b97482871926fe04a377b679b00b418093
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-16 21:08:22 +0800

    add attempt at implementing peek()

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -2,7 +2,10 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 fn main() {
-    println!("Implement me!");
+    let stack = GlobalStack(Arc::new(Mutex::new(vec![82])));
+    let stack2 = stack.clone();
+    stack.push(93);
+    stack2.push(23);
 }
 
 struct GlobalStack<T>(Arc<Mutex<Vec<T>>>);
@@ -16,6 +19,12 @@ impl<T> GlobalStack<T> {
     pub fn pop(&self) -> Option<T> {
         let mut vector = self.0.lock().unwrap();
         vector.pop()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        let vector = self.0.lock().unwrap();
+        // Это конечно же не работает
+        vector.get(vector.len())
     }
 
     pub fn clone(&self) -> Self {
