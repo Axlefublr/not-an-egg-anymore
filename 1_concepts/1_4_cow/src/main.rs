1abae33bfb7211c2153ec40b2481a33faaa0f869
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-10-17 20:15:30 +0800

    add first implementation

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use std::borrow::Cow;
+use std::env;
+
+const DEFAULT_PATH: &str = "/etc/app/app.conf";
+const ENV_CONF: &str = "APP_CONF";
+
 fn main() {
-    println!("Implement me!");
+    let conf: Cow<'_, str> = {
+        if let Some(flag) = Args::parse().conf {
+            Cow::Owned(flag)
+        } else if matches!(env::var(ENV_CONF), Ok(env_conf) if !env_conf.is_empty()) {
+            Cow::Owned(env::var(ENV_CONF).unwrap())
+        } else {
+            Cow::Borrowed(DEFAULT_PATH)
+        }
+    };
+    println!("{conf}");
+}
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    conf: Option<String>,
 }
