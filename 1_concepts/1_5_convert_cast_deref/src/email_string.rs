13ae22c6fca7ee09e396bf277af0e8da5d8fa5cb
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-09 15:11:35 +0800

    intermediate changes

--- 1_concepts/1_5_convert_cast_deref/src/email_string.rs
+++ 1_concepts/1_5_convert_cast_deref/src/email_string.rs
@@ -1,1 +1,48 @@
+use std::ops::Deref;
+use std::ops::DerefMut;
 
+use regex::Regex;
+
+const VALID_EMAIL: &str = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+pub struct EmailString(String);
+
+impl TryFrom<String> for EmailString {
+    type Error = &'static str;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let regex = Regex::new(VALID_EMAIL).unwrap();
+        if regex.is_match(&value) {
+            Ok(EmailString(value))
+        } else {
+            Err("invalid email")
+        }
+    }
+}
+
+impl TryFrom<&str> for EmailString {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let regex = Regex::new(VALID_EMAIL).unwrap();
+        if regex.is_match(value) {
+            Ok(EmailString(value.to_owned()))
+        } else {
+            Err("invalid email")
+        }
+    }
+}
+
+impl Deref for EmailString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for EmailString {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
--- 1_concepts/1_5_convert_cast_deref/src/random_pointer.rs
+++ 1_concepts/1_5_convert_cast_deref/src/random_pointer.rs
@@ -1,1 +1,24 @@
+use std::ops::Deref;
+use rand::Rng;
 
+pub struct Random<T>(T, T, T);
+
+impl<T> Random<T> {
+    pub fn new(one: T, two: T, three: T) -> Self {
+        Random(one, two, three)
+    }
+}
+
+impl<T> Deref for Random<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(0..=2) {
+            0 => &self.0,
+            1 => &self.1,
+            2 => &self.2,
+            _ => unreachable!("The range is from 0 to 2, inclusive. All of them are taken care of.")
+        }
+    }
+}
